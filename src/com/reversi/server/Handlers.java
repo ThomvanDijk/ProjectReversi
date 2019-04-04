5cfa5c9941b1036267105701eeca1d44f0d42fbf
Dennis van der Zwan <dennisvanderzwan@MacBook-Pro-van-Dennis.local>
2019-04-04 13:02:58 +0200

    Handler enum

--- src/com/reversi/server/Handlers.java
+++ src/com/reversi/server/Handlers.java
@@ -1,5 +1,26 @@
 package com.reversi.server;
 
-public class Handlers {
+public enum Handlers {
 
+	HELP,		
+	GAME,		
+	MATCH,		
+	YOURTURN,	
+	MOVE,		
+	CHALLENGE,	
+	WIN,			
+	LOSS,		
+	DRAW,
+	login,
+	logout,
+	get,
+	gamelist,
+	playerlist,
+	subscribe,
+	move,
+	challenge,
+	accept,
+	forfeit,
+	help;
+	
 }
