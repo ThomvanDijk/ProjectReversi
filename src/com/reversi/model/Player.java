02a5e928f479f5037ed853fc7daafc50995d759f
Thom van Dijk <thom.a.vandijk@gmail.com>
2019-04-16 23:59:10 +0200

    Added JavaDoc to Player

--- src/com/reversi/model/Player.java
+++ src/com/reversi/model/Player.java
@@ -1,11 +1,19 @@
 package com.reversi.model;
 
+/**
+ * This class will save everything that has something to do with a player.
+ * 
+ * @author  Thom van Dijk
+ * @author  Sebastiaan van Vliet
+ * @version 1.0
+ * @since   12-04-2019
+ */
 public class Player {
-	
+
 	public enum PlayerType {
 		HUMAN, AI, SERVER
 	}
-	
+
 	protected final PlayerType type;
 	protected final int color;
 	private int score;
@@ -20,27 +28,26 @@ public class Player {
 		score = 0;
 		name = "Computer";
 		hasTurn = false;
-		
-		if (color == 1){
+
+		if (color == 1) {
 			opponent = 2;
-		}
-		else {
+		} else {
 			opponent = 1;
 		}
-		
-		if(type.equals(PlayerType.AI)) {
+
+		if (type.equals(PlayerType.AI)) {
 			ai = new AI();
 		}
 	}
-	
+
 	public PlayerType getType() {
 		return type;
 	}
-	
+
 	public int getColor() {
 		return color;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
@@ -52,19 +59,19 @@ public class Player {
 	public int getScore() {
 		return score;
 	}
-	
+
 	public void setScore(int score) {
 		this.score = score;
 	}
-	
+
 	public void setTurn(boolean hasTurn) {
 		this.hasTurn = hasTurn;
 	}
-	
+
 	public boolean hasTurn() {
 		return hasTurn;
 	}
-	
+
 	public void incrementScore() {
 		score++;
 	}
